3a4baf15fc420e6e6f384558c87e0ad6c97d2882
JoonGabbay <gabbayjoon@gmail.com>
2024-07-23 15:59:53 -0400

    made robot move
    Finn has rest of code from day 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -1,15 +1,30 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Drivetrain {
     //Declare Motors
-
-
-    public Drivetrain() {
+DcMotor bl;
+DcMotor br;
+DcMotor fl;
+DcMotor fr;
+    public Drivetrain(HardwareMap hardwareMap) {
         //Instantiate motors
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     //Callable drive functions
-    public void drive(){
-
+    public void drive(double drive){
+        bl.setPower(drive);
+        br.setPower(drive);
+        fl.setPower(drive);
+        fr.setPower(drive);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+
 @TeleOp(name="Iterative TeleOp", group="Iterative Opmode")
 public class IterativeTeleOp extends OpMode {
 
@@ -12,11 +15,13 @@ public class IterativeTeleOp extends OpMode {
 
     //Timer
     ElapsedTime runtime = new ElapsedTime();
+    Drivetrain dt;
 
     @Override
     public void init() {
         //Set timer to 0
         runtime.reset();
+        dt = new Drivetrain(hardwareMap);
         //Code that runs when you hit init
     }
 
@@ -28,14 +33,15 @@ public class IterativeTeleOp extends OpMode {
     @Override
     public void loop() {
         //Code that *LOOPS* after you hit start
+        dt.drive(gamepad1.right_stick_y);
     }
 
     @Override
     public void stop(){
         //Code that runs when you hit stop
 
-        multTelemetry.addData("Runtime", runtime);
-        multTelemetry.update();
+        telemetry.addData("Runtime", runtime);
+        telemetry.update();
 
     }
 
