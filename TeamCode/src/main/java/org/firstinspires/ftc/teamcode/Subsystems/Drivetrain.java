eeda2255b97e56b4fd19c7b3f8d01a1c03bb11ba
JGcode126 <2008jerrygong@gmail.com>
2024-07-24 12:14:55 -0400

    Driver orianted (thank god)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.zLibraries.Utilities.Vector2d;
 
 public class Drivetrain {
     //Declare Motors
@@ -23,6 +28,9 @@ public class Drivetrain {
         motorbr.setDirection(DcMotor.Direction.REVERSE);
     }
 
+
+
+
     //Callable drive functions
     public void drive(double drive, double strafe, double turn, double slow){
 
@@ -38,4 +46,29 @@ public class Drivetrain {
             motorbr.setPower((drive + strafe - turn) * -0.75);
         }
     }
-}
+
+    public void driveDO(double drive, double strafe, double turn, double slow, double heading){
+        Vector2d driveVector = new Vector2d(strafe, drive);
+        Vector2d rotatedVector = driveVector.rotate(Math.toRadians(heading));
+
+        drive = rotatedVector.y;
+        strafe = rotatedVector.x;
+
+
+
+        if (slow > 0.05){
+            motorfl.setPower((drive + strafe + turn) * -0.25);
+            motorfr.setPower((drive - strafe - turn) * -0.25);
+            motorbl.setPower((drive - strafe + turn) * -0.25);
+            motorbr.setPower((drive + strafe - turn) * -0.25);
+        } else {
+            motorfl.setPower((drive + strafe + turn) * -0.75);
+            motorfr.setPower((drive - strafe - turn) * -0.75);
+            motorbl.setPower((drive - strafe + turn) * -0.75);
+            motorbr.setPower((drive + strafe - turn) * -0.75);
+        }
+
+        }
+    }
+
+
