776cc4706945d4700ea4c1d26db8ec4e62b0193a
Thaddeus Bardsley <thaddeusbardsley2@gmail.com>
2024-07-24 09:06:16 -0400

    ghjk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -1,15 +1,30 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Drivetrain {
     //Declare Motors
+    DcMotor fr;
+    DcMotor fl;
+    DcMotor br;
+    DcMotor bl;
 
-
-    public Drivetrain() {
+    public Drivetrain(HardwareMap hardwareMap) {
         //Instantiate motors
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+
     }
 
     //Callable drive functions
-    public void drive(){
+    public void driveForward(){
+        fr.setPower(1);
+        fl.setPower(1);
+        br.setPower(1);
+        bl.setPower(1);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/IterativeTeleOp.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
 @TeleOp(name="Iterative TeleOp", group="Iterative Opmode")
 public class IterativeTeleOp extends OpMode {
 
@@ -12,30 +14,35 @@ public class IterativeTeleOp extends OpMode {
 
     //Timer
     ElapsedTime runtime = new ElapsedTime();
+    Drivetrain dt;
 
     @Override
     public void init() {
         //Set timer to 0
         runtime.reset();
+        dt = new Drivetrain(hardwareMap);
         //Code that runs when you hit init
     }
 
     @Override
     public void start(){
         //Code that runs when you hit start
+
+        dt.driveForward();
     }
 
     @Override
     public void loop() {
         //Code that *LOOPS* after you hit start
+        dt.driveForward(gamepad1.left_stick_y);
     }
 
     @Override
     public void stop(){
         //Code that runs when you hit stop
 
-        multTelemetry.addData("Runtime", runtime);
-        multTelemetry.update();
+        telemetry.addData("Runtime", runtime);
+        telemetry.update();
 
     }
 
