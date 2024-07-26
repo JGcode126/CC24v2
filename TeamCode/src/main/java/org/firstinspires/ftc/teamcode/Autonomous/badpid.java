373ca848095714d55f46d94fbe3db783736a60bc
iamaren <157918963+iamaren@users.noreply.github.com>
2024-07-26 13:46:51 -0400

    minor stuff
    pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/badpid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/badpid.java
@@ -4,12 +4,23 @@ public class badpid {
     private double proportional;
     //private double integral;
     //private double derivative;
+
+    //private double integralSum;
+
+    //private boolean isTuning = true;
+
     private double currentError;
     private double lastError;
     private double deltaError = currentError-lastError;
 
-    public badpid
+    public badpid(double proportional/*, double integral, double derivative*/){
+        this.proportional = proportional;
+        //this.integral = integral;
+        //this.derivative = derivative;
+    }
+
+
+    }
 
 
 
-}
