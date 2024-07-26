04050fdce041d2b2ce720294a8639a671e639b83
iamaren <157918963+iamaren@users.noreply.github.com>
2024-07-26 10:06:23 -0400

    started pid
    start of crappy pid that i wont use

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/badpid.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+public class badpid {
+    private double proportional;
+    //private double integral;
+    //private double derivative;
+    private double currentError;
+    private double lastError;
+    private double deltaError = currentError-lastError;
+
+    public badpid
+
+
+
+}
