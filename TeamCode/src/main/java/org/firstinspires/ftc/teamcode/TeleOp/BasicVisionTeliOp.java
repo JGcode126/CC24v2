c392b72b35d1fae6da502e0bda6be8848b5b88b2
AlexX <idostuff193@gmail.com>
2024-08-06 14:26:58 -0400

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BasicVisionTeliOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BasicVisionTeliOp.java
@@ -1,44 +1,50 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
 
-import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.hardwareMap;
-import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.telemetry;
+import android.util.Size;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 //import org.firstinspires.ftc.teamcode.Autonomous.BaseOpMode;
 import org.firstinspires.ftc.teamcode.Vision.BasicVisionProcessor;
-import org.firstinspires.ftc.teamcode.Autonomous.BaseOpMode;
-import org.firstinspires.ftc.teamcode.Vision.BasicVisionProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 
 
 @TeleOp(name="AAAA vision", group="Iterative Opmode")
-public class BasicVisionTeliOp extends BaseOpMode {
+public class BasicVisionTeliOp extends OpMode {
     private BasicVisionProcessor visionProcessor = new BasicVisionProcessor();
     private VisionPortal visionPortal;
     private WebcamName webcam1;
-    @Override
-    public void externalInit() {
+    Size size = new Size(1280, 720);
+
+    public void init() {
         telemetry.update();
         webcam1 = hardwareMap.get(WebcamName.class, "Webcam 1");
         visionPortal = new VisionPortal.Builder()
                 //setup for using webcam, there is a different way to set up a phone camera
                 .setCamera(webcam1)
+                .setCameraResolution(size)
                 .addProcessor(visionProcessor)
                 .build();
+
+
         FtcDashboard.getInstance().startCameraStream(visionProcessor, 0);
 
-        waitForStart();
+    }
+@Override
+   public void start() {
+        System.out.println("im slowly losing sanity");
     }
 
     @Override
-    public void externalLoop() {
-
+    public void loop() {
 
     }
+
+
 }
