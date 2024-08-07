029d90770e703f3a8a19f49c602763fbe2970903
iamaren <157918963+iamaren@users.noreply.github.com>
2024-08-07 14:00:18 -0400

    odo cuddle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CuddleOpMode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.roboctopi.cuttlefish.controller.Waypoint;
+import com.roboctopi.cuttlefish.queue.CustomTask;
+import com.roboctopi.cuttlefish.queue.DelayTask;
+import com.roboctopi.cuttlefish.queue.PointTask;
+import com.roboctopi.cuttlefish.utils.Pose;
+
+import org.firstinspires.ftc.teamcode.cuddlefish.CuddleInitIDK;
+
+
+@TeleOp(name="Example Field-Centric Driver Encoders", group="Example ")
+public class CuddleOpMode extends CuddleInitIDK {
+    public void onInit() {
+        super.onInit();
+    }
+    public void main() {
+        super.main();
+
+    }
+
+    public void mainLoop()
+    {
+        super.mainLoop();
+
+        encoderLocalizer.update();
+        System.out.println(encoderLocalizer.getPos());
+        telemetry.addData("Localizer X:",encoderLocalizer.getPos().getX());
+        telemetry.addData("Localizer Y:",encoderLocalizer.getPos().getY());
+        telemetry.addData("Localizer R:",encoderLocalizer.getPos().getR());
+        telemetry.update();
+    }
+}
