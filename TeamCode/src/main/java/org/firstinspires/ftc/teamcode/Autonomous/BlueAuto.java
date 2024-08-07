1f54fb7aaee1220a562674d58ca5529e6dcb2aa2
iamaren <157918963+iamaren@users.noreply.github.com>
2024-08-07 09:29:17 -0400

    l

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAuto.java
@@ -23,10 +23,7 @@ public class BlueAuto extends LinearOpMode {
         if(opModeIsActive()){
             //Run Auto
 
-            while(opModeIsActive()){
 
-
-            }
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuto.java
@@ -23,10 +23,7 @@ public class RedAuto extends LinearOpMode {
         if(opModeIsActive()){
             //Run Auto
 
-            while(opModeIsActive()){
-
-
-            }
+            
 
         }
     }
