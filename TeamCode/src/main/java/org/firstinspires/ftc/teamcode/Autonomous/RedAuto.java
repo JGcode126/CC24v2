5d8790f05f0e0e4819425907273528c26e52984d
Thaddeus Bardsley <thaddeusbardsley2@gmail.com>
2024-08-09 14:02:10 -0400

    HARDCORE IM HARDCORE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAuto.java
@@ -1,11 +1,10 @@
-
-
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.multTelemetry;
 import static org.firstinspires.ftc.teamcode.Utilities.OpModeUtils.setOpMode;
 import static org.firstinspires.ftc.teamcode.Vision.BasicVisionProcessor.largestRect;
 
+import android.graphics.Canvas;
 import android.util.Size;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -16,32 +15,48 @@ import com.roboctopi.cuttlefish.queue.PointTask;
 import com.roboctopi.cuttlefish.utils.Pose;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.teamcode.Vision.BasicVisionProcessor;
 import org.firstinspires.ftc.teamcode.Vision.blueProcessor;
 import org.firstinspires.ftc.teamcode.Vision.redProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+import org.firstinspires.ftc.teamcode.Vision.blueProcessor;
 
-@Autonomous(name = "RedA")
+import java.util.ArrayList;
+@Autonomous(name = "REDA")
+
 public class RedAuto extends CuddleOpMode {
 
     // Declare Subsystems
 
+
     redProcessor redProcessor = new redProcessor();
     private VisionPortal visionPortal;
+    //AprilTagProcessor aprilTagProcessor;
     private WebcamName webcam1;
-    Size size = new Size(1280, 720);
+    Size size = new Size(1280,720);
+
 
 
     public enum VisionState {
         RIGHT, MIDDLE, LEFT, NODETECT
     }
-
     VisionState currentVisionState = VisionState.NODETECT;
 
-    public void initialize() {
+    public void onInit(){
+        super.onInit();
         // Initialize Subsystems
         setOpMode(this);
         telemetry.update();
+        //aprilTagProcessor = new AprilTagProcessor.Builder()
+//                .setLensIntrinsics(601.169,601.169,278.814,259.214)
+//                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+//                .build();
         webcam1 = hardwareMap.get(WebcamName.class, "Webcam 1");
         visionPortal = new VisionPortal.Builder()
                 //setup for using webcam, there is a different way to set up a phone camera
@@ -55,73 +70,72 @@ public class RedAuto extends CuddleOpMode {
         visionPortal.resumeStreaming();
         visionPortal.resumeLiveView();
 
-        if (redProcessor.returnPos() == 1) {
+        if (org.firstinspires.ftc.teamcode.Vision.redProcessor.returnPos() == 1){
+            //currentVisionState = VisionState.LEFT;
             currentVisionState = VisionState.LEFT;
-        } else if (redProcessor.returnPos() == 2) {
+        } else if (org.firstinspires.ftc.teamcode.Vision.redProcessor.returnPos() == 2) {
             currentVisionState = VisionState.MIDDLE;
-        } else if (redProcessor.returnPos() == 3) {
+        } else if (org.firstinspires.ftc.teamcode.Vision.redProcessor.returnPos() == 3) {
+            //currentVisionState = VisionState.RIGHT;
             currentVisionState = VisionState.RIGHT;
         } else {
-            currentVisionState = VisionState.NODETECT;
+            currentVisionState = VisionState.MIDDLE;
         }
     }
 
 
-    @Override
-    public void runOpMode() {
-        initialize();
+    public void mainLoop() {
+        super.mainLoop();
 
-        waitForStart();
-        if (opModeIsActive()) {
-            //Run Auto
+        //Run Auto
 
-            multTelemetry.addData("current state", currentVisionState);
-            switch (currentVisionState) {
-                case LEFT:
+        multTelemetry.addData("current state", currentVisionState);
+        switch (currentVisionState) {
+            case LEFT:
 
-                    if (tasksQueued == false) {
-                        queue.addTask(new PointTask(new Waypoint(new Pose(0.0, 609.6, 0.0), 0.5), ptpController));
-                        queue.addTask(new PointTask(new Waypoint(new Pose(606.6, 0.0, 0.0), 0.5), ptpController));
-                    }
-                    tasksQueued = true;
+                if(!tasksQueued){
+                    queue.addTask(new PointTask(new Waypoint(new Pose(0.0,1750.6,0.0),0.5), ptpController));
+                    queue.addTask(new PointTask(new Waypoint(new Pose(-6106.6,1750.6,0.0),0.5), ptpController));
+                }
+                tasksQueued = true;
 
 
-                    //robot should park in the leftmost square
-                    break;
+                //robot should park in the leftmost square
+                break;
 
-                case MIDDLE:
+            case MIDDLE:
 
-                    if (tasksQueued == false) {
-                        queue.addTask(new PointTask(new Waypoint(new Pose(0.0, 609.6, 0.0), 0.5), ptpController));
-                    }
-                    tasksQueued = true;
+                if(!tasksQueued){
+                    queue.addTask(new PointTask(new Waypoint(new Pose(900.0,900,0.0),0.5), ptpController));
+                }
+                tasksQueued = true;
 
-                    //robot should park in the middle
-                    break;
+                //robot should park in the middle
+                break;
 
-                case RIGHT:
+            case RIGHT:
 
-                    if (tasksQueued == false) {
-                        queue.addTask(new PointTask(new Waypoint(new Pose(0.0, 609.6, 0.0), 0.5), ptpController));
-                        queue.addTask(new PointTask(new Waypoint(new Pose(-606.6, 0.0, 0.0), 0.5), ptpController));
-                    }
-                    tasksQueued = true;
+                if(!tasksQueued){
 
-                    //robot should park to the left
-                    break;
+                    //jank af
+                    queue.addTask(new PointTask(new Waypoint(new Pose(0.0,1750.6,0.0),0.5), ptpController));
+                    queue.addTask(new PointTask(new Waypoint(new Pose(-6106.6,1750.6,0.0),0.5), ptpController));
 
-                case NODETECT:
+                }
+                tasksQueued = true;
 
-                    if (tasksQueued == false) {
-                        queue.addTask(new PointTask(new Waypoint(new Pose(0.0, 609.6, 0.0), 0.5), ptpController));
-                    }
-                    tasksQueued = true;
+                //robot should park to the left
+                break;
 
-                    //robot should park in the middle so we have some chance of scoring
-                    break;
-            }
+            case NODETECT:
 
+                if(tasksQueued == false){
+                    queue.addTask(new PointTask(new Waypoint(new Pose(900.0,900,0.0),0.5), ptpController));
+                }
+                tasksQueued = true;
 
+                //robot should park in the middle so we have some chance of scoring
+                break;
         }
     }
 }
