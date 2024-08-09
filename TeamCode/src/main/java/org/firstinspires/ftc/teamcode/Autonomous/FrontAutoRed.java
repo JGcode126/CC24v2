8a1925619f03f10f51b43e31315369579d0ee1d4
JoonGabbay <gabbayjoon@gmail.com>
2024-08-09 12:41:42 -0400

    yeetuturyue4ue5hg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FrontAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FrontAutoRed.java
@@ -4,20 +4,28 @@ import static org.firstinspires.ftc.teamcode.Autonomous.AAA_Paths.Path.RedAuotoF
 import static org.firstinspires.ftc.teamcode.Autonomous.AAA_Paths.Path.RedAutoFrontForward;
 import static org.firstinspires.ftc.teamcode.Autonomous.AAA_Paths.Path.RedAutoFrontGetRings;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.KCP.Movement;
 import org.firstinspires.ftc.teamcode.Subsystems.Scoring;
 import org.firstinspires.ftc.teamcode.Utilities.DashConstants.AutoDash;
+import org.firstinspires.ftc.teamcode.Vision.BasicVisionProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
 
-@Autonomous(name="Front auto blue", group="Op mode")
+@Autonomous(name="Front auto red", group="Op mode")
 public class FrontAutoRed extends BaseOpMode {
     AAA_Paths.Path path;
     Movement drive;
     Scoring scoring;
     ElapsedTime timer;
     boolean pathDone = false;
+    private BasicVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+    private WebcamName webcam1;
+    int parkPos;
     @Override
     public void externalInit() {
         drive = new Movement(-6.3,109,Math.toRadians(90));
@@ -28,11 +36,22 @@ public class FrontAutoRed extends BaseOpMode {
         RedAutoFrontForward.compile();
         RedAutoFrontGetRings.compile();
         RedAuotoFrontDepositRings.compile();
+        webcam1 = hardwareMap.get(WebcamName.class, "Webcam 1");
+        visionProcessor = new BasicVisionProcessor();
+
+        visionPortal = new VisionPortal.Builder()
+                //setup for using webcam, there is a different way to set up a phone camera
+                .setCamera(webcam1)
+                .addProcessor(visionProcessor)
+                .build();
+        FtcDashboard.getInstance().startCameraStream(visionProcessor, 0);
+        waitForStart();
     }
 
     @Override
     public void externalInitLoop(){
         scoring.setArmState(Scoring.ArmSwitchStatement.UPCLOSED);
+        parkPos=visionProcessor.whichThird();
     }
 
 
